bd055762feb5f586681e22405681685ddd6093b3
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2020-05-13 01:14:17 -0500

    Commit inicial

---
+++ cliente.py
@@ -0,0 +1,73 @@
+import socket
+import struct
+import numpy as np
+import cv2
+from PIL import Image
+from sys import getsizeof
+
+#Configuración inicial del socket.
+grupo_multicast = '224.1.1.1'
+info_servidor = ('', 10000)
+
+#Se crea el socket UDP (Recordemos que el multicast es implementado con socket UDP)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(info_servidor)
+
+group = socket.inet_aton(grupo_multicast)
+mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+for i in range(1, 7):
+    num_img = i
+    info_imagen_recibida = False
+
+    while(not info_imagen_recibida):
+        print("Esperando mensaje... ")
+        try:
+            mensaje_recibido, direccion_envio = sock.recvfrom(128)
+            print("Tamanio cabecera: ", getsizeof(mensaje_recibido), " bytes.")
+        except:
+            print("El servidor está a la mitad del envío de otra imagen, esperando a que termine...")
+        else:
+            info_imagen_recibida = True
+
+    """print("Esperando mensaje... ")
+    mensaje_recibido, direccion_envio = sock.recvfrom(1024)
+    print("Tamanio cabecera: ", getsizeof(mensaje_recibido), " bytes.")"""
+
+    print("Enviando ACK a ", direccion_envio)
+    sock.sendto(bytes('ACK', 'utf8'), direccion_envio)
+
+    informacion_imagen = mensaje_recibido.decode().split('_')
+    num_partes = informacion_imagen[0]
+    tamanio_buffer = informacion_imagen[1]
+    extension_img = informacion_imagen[2]
+
+    print("Num partes:", num_partes)
+    print("Tamanio buffer: ", tamanio_buffer)
+
+    lista_segmentos = []
+
+    for i in range(int(num_partes)):
+        mensaje_recibido, direccion_envio = sock.recvfrom(int(tamanio_buffer))
+        lista_segmentos.append(mensaje_recibido)
+
+        print("Enviando ACK a ", direccion_envio)
+        sock.sendto(bytes('ACK', 'utf8'), direccion_envio)
+
+    #info_bytes_img = ''.split(lista_segmentos)
+
+    print("Informacion: ", lista_segmentos[0])
+    print("--------------------")
+    print("Informacion: ", lista_segmentos[1])
+
+    info_bytes_completa = b''
+    for segmento in lista_segmentos:
+        info_bytes_completa += segmento
+
+    arreglo_img = np.frombuffer(info_bytes_completa, np.uint8)
+    nueva_img = cv2.imdecode(arreglo_img, cv2.IMREAD_COLOR)
+
+    print("Guardando imagen...")
+    nueva_img_formateada = Image.fromarray(nueva_img)
+    nueva_img_formateada = nueva_img_formateada.save("imgDescargada/clienteImg"+str(num_img)+"."+extension_img)
---
+++ servidor.py
@@ -0,0 +1,96 @@
+import socket
+import struct
+import os
+from PIL import Image
+import numpy as np
+import cv2
+import sys
+from time import sleep
+from math import ceil
+
+ruta_imagenes = 'img/'
+
+#--------------------------------------------------
+#Configuración inicial del socket para multicast.
+grupo_multicast = ('224.1.1.1', 10000)
+#Se crea el socket UDP (Recordemos que el multicast se implementa con UDP)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(0.2)
+ttl = struct.pack('b', 1)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+lista_imagenes = os.listdir(ruta_imagenes)
+
+for num_img, nombre_img in enumerate(lista_imagenes):
+    print(nombre_img)
+    extension = nombre_img.split('.')[1]
+    print("Extension:", extension)
+
+    #Se abre la imagen.
+    img = Image.open(ruta_imagenes+'img'+str(num_img+1)+'.'+extension)
+    #Se obtiene la matriz de pixeles que representa la imagen.
+    matriz_imagen = np.asarray(img)
+    #Se convierte la matriz a una representación en bytes. (Serialización)
+    img_ser = cv2.imencode('.'+extension, matriz_imagen)[1].tostring()
+
+    tamanio_bytes_img = sys.getsizeof(img_ser) 
+    num_caracteres = len(img_ser)
+    print("Tamanio: ", tamanio_bytes_img, " bytes.")
+    print("Num. caracteres: ", num_caracteres, " caracteres.")
+
+    is_tamanio_valido = False
+    while(not is_tamanio_valido):
+        num_partes = int(input("Partes en las que se dividirá la imagen: "))
+        tamanio_buffer = ceil(tamanio_bytes_img/num_partes)
+        #2**16 es el tamaño máximo del buffer. 128 es el tamaño máximo para la cabecera inicial de información.
+        if( tamanio_buffer < 2**16 and tamanio_buffer > 128): 
+            print("Número de partes válido.")
+            is_tamanio_valido = True
+        else:
+            print("Número de partes inválido. Intenta de nuevo.")
+    
+    informacion_imagen = str(num_partes)+'_'+str(tamanio_buffer)+'_'+extension
+
+    try:
+        enviado = sock.sendto(informacion_imagen.encode(), grupo_multicast)
+        while(True):
+            print(sys.stderr, "Esperando respuestas...")
+            try:
+                mensaje_recibido, emisor = sock.recvfrom(1024)
+            except socket.timeout:
+                print(sys.stderr, "Tiempo de recepción de respuestas finalizado.")
+                break
+            else:
+                print(sys.stderr, "", mensaje_recibido, " de: ", emisor)
+    except ValueError:
+        print("Error: ", ValueError)
+
+    sleep(2)
+
+    aux_posicion_buffer_img = 0
+    for i in range(num_partes):
+        if(aux_posicion_buffer_img+tamanio_buffer > tamanio_bytes_img):
+            segmento_a_enviar_img = img_ser[aux_posicion_buffer_img:]
+        else:
+            segmento_a_enviar_img = img_ser[aux_posicion_buffer_img:aux_posicion_buffer_img+tamanio_buffer]
+
+        try:
+            enviado = sock.sendto(segmento_a_enviar_img, grupo_multicast)
+            while(True):
+                print(sys.stderr, "Esperando respuestas...")
+                try:
+                    mensaje_recibido, emisor = sock.recvfrom(1024)
+                except socket.timeout:
+                    print(sys.stderr, "Tiempo de recepción de respuestas finalizado.")
+                    break
+                else:
+                    print(sys.stderr, "", mensaje_recibido, " de: ", emisor)
+        except ValueError:
+            print("Error: ", ValueError)
+
+        aux_posicion_buffer_img += tamanio_buffer
+
+    sleep(5)
+
+print(sys.stderr, "Cerrando socket...")
+sock.close()
